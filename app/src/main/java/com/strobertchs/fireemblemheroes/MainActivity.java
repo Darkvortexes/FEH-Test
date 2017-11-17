f0d8631114a4600165ea5e69b98e2dae49a8b9da
Vince Phung <vphung2000@gmail.com>
2017-11-17 01:02:21 -0500

    Updated menu screen and progress on HeroesActivity

--- app/src/main/java/com/strobertchs/fireemblemheroes/MainActivity.java
+++ app/src/main/java/com/strobertchs/fireemblemheroes/MainActivity.java
@@ -1,13 +1,25 @@
 package com.strobertchs.fireemblemheroes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonHeroes = (Button)findViewById(R.id.buttonHeroes);
+        buttonHeroes.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this, HeroesActivity.class);
+        startActivity(i);
     }
 }
