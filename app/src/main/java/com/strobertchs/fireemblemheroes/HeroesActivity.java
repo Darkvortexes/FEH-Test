c0a80f85819e5dfa1495b71c7851a75d1897ca2f
Vince Phung <vphung2000@gmail.com>
2017-11-17 20:26:23 -0500

    Completed search bar for HeroesActivity

--- app/src/main/java/com/strobertchs/fireemblemheroes/HeroesActivity.java
+++ app/src/main/java/com/strobertchs/fireemblemheroes/HeroesActivity.java
@@ -2,12 +2,56 @@ package com.strobertchs.fireemblemheroes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HeroesActivity extends AppCompatActivity {
 
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heroes);
+        ListView lv_heroes = (ListView)findViewById(R.id.listViewHeroes);
+        ArrayList<String> arrayHeroes = new ArrayList<>();
+        arrayHeroes.addAll(Arrays.asList(getResources().getStringArray(R.array.array_heroes)));
+
+        adapter = new ArrayAdapter<>(
+                HeroesActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayHeroes);
+        lv_heroes.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.heroes_search,menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query){
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText){
+                adapter.getFilter().filter(newText);
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
